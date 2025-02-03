46c12c66f47a918014d5a2b4482f79905a974272
MouhebN <mouheb.naddari@esprit.tn>
2025-02-03 11:38:38 +0100

    initialisation

---
+++ src/main/java/com/bramaLog/BramaLogApplication.java
@@ -0,0 +1,13 @@
+package com.bramaLog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BramaLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BramaLogApplication.class, args);
+	}
+
+}
